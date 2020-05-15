9c8b31a927d1393f9dbf53bd21a4e7d06277fd2a
Gonçalo Ferreira <gonmf@sapo.pt>
2020-05-15 23:34:14 +0100

    Add tests

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# rm tests/*.output
+
 for i in ./tests/*.input; do
+  output_file=${i%.input}.output
   test_name=${i%.input}
   test_name=${test_name#./}
   echo "Test ${test_name}"
 
-  diff -y --suppress-common-lines <(./dakilang -c $i) ${i%.input}.output || (echo "Test ${test_name} FAILED" && exit 1)
+  # To create all .output files
+  # ./dakilang -c $i > $output_file
+
+  diff -y --suppress-common-lines <(./dakilang -c $i) $output_file || (echo "Test ${test_name} FAILED" && exit 1)
 done
