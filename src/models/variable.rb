1b7ef3bd82b65ec18a358b687ab08a92e27ea128
gonmf <gonmf@sapo.pt>
2020-07-25 22:28:30 +0100

    Minor improvement

--- src/models/variable.rb
+++ src/models/variable.rb
@@ -43,7 +43,7 @@ module DakiLang
 
             "#{name} #{condition == '!=' ? '<>' : condition} #{value}"
           else
-            "#{name}"
+            name
           end
         end
       end
